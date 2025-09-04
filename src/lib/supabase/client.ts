4b84d8f62802cad632c128aebd4d616206c11e3d
AroshaEpitcore <arosha@epitcore.com>
2025-09-04 14:03:30 +0530

    env changes

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,20 +1,7 @@
-// "use client";
-// import { createBrowserClient } from "@supabase/ssr";
-// export const createClient = () =>
-//   createBrowserClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-//   );
-// lib/supabase/client.ts
-import { createClient as createClientSupabase } from "@supabase/supabase-js";
-
-export const createClient = () => {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  if (!url || !key) {
-    throw new Error("Supabase URL or key is missing!");
-  }
-
-  return createClientSupabase(url, key);
-};
+"use client";
+import { createBrowserClient } from "@supabase/ssr";
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
